2db7e55fa180297c20f786e1b7c1fbe60b771615
Andrii Sachuk <ansachuk@icloud.com>
2026-09-11 11:12:21 +0200

    deleted evrth from Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,5 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] matrix = {{0, 1, 2}, {4, 5, 6}, {1, 1, 1}};
-
-        int[] diagonal = MatrixDiagonal.getDiagonal(matrix);
-        int[] counterDiagonal = MatrixDiagonal.getCounterDiagonal(matrix);
-
-        System.out.println("diagonal = " + Arrays.toString(diagonal));
-        System.out.println("counterDiagonal = " + Arrays.toString(counterDiagonal));
     }
 }
